0dcdcdd6c10e4642c201b635a39735b50a30436e
kimhanmin <window12ty@naver.com>
2025-12-31 21:52:45 +0900

    Track backend as normal directory

---
+++ backend/src/main/java/com/hows/alphahows/AlphaHowsApplication.java
@@ -0,0 +1,13 @@
+package com.hows.alphahows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaHowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaHowsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/hows/alphahows/AlphaHowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hows.alphahows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaHowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
